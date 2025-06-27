078a40476c306eefaa3e78ccc666a10d43c2f45b
VelasquezE <132410679+VelasquezE@users.noreply.github.com>
2025-06-27 14:39:48 -0500

    Reorganize structure, add image counter

--- organizadorArchivos_descargas.py
+++ organizadorArchivos_descargas.py
@@ -21,7 +21,11 @@ extensions_type = {
     'temporal': ['log', 'bak', 'tmp', 'part', 'crdownload']
 }
 
-def get_extension(file_path):
+def get_files_path_list(directory_path):
+    file_paths_list  = [p for p in glob.glob(directory_path + "**", recursive = True) if os.path.isfile(p)]   
+    return file_paths_list
+
+def get_file_extension(file_path):
     return os.path.basename(file).split(".", 1)[1]
 
 def get_document_type(file_path, extensions_type_dict):
@@ -29,7 +33,7 @@ def get_document_type(file_path, extensions_type_dict):
     Gets document type comparing the extension to a clasification
     given in a dictionary.
     """
-    extension = get_extension(file_path)
+    extension = get_file_extension(file_path)
 
     for type, extensions in extensions_type_dict.items():
         if extension in extensions:
@@ -39,10 +43,9 @@ def get_document_type(file_path, extensions_type_dict):
     
     return extension
     
-
 def is_file_old(file_path, maximum_days = 120):
     """
-    Checks if the last modification time is greater than 
+    Checks if the modification time is greater than 
     the maximum days.
     """
     last_modification = os.path.getmtime(file_path)
@@ -54,48 +57,82 @@ def is_file_old(file_path, maximum_days = 120):
     else:
         return False
 
-def give_new_filename(file_type, file_path):
+def give_modification_date(file_path):
+    """
+    Gives the modification date of a file (Year- month-day)
+    Returns:
+        modification_date_string (String)
+    """
+    modification_date = datetime.date.fromtimestamp(os.path.getmtime(file))
+    modification_date_string = modification_date.strftime("%Y-%m-%d")
+
+    return modification_date_string
+
+def give_new_filename(file_type, file_path, n_image):
     """
     Creates new filename: 
         <type>_<description>_<YYYY-MM-DD>.ext
+        For images:
+        <type>_<YYYY-MM-DD>.ext
     Returns:
         new_filename (string)
     """
-    creation_date = datetime.date.fromtimestamp(os.path.getctime(file))
-    creation_date_string = creation_date.strftime("%Y-%m-%d")
+    modification_date = give_modification_date(file_path)
 
     current_filename = os.path.basename(file_path).split(".")[0]
-    extension = get_extension(file_path)
+    extension = get_file_extension(file_path)
 
     if file_type == "imagen":
-        new_filename = f"{file_type}_{creation_date_string}.{extension}"
-        #TODO:definir un contador para imagenes del mismo día
+        new_filename = f"{file_type}_0{n_image}_{modification_date}.{extension}"
     else:
-        new_filename = f"{file_type}_{current_filename}_{creation_date_string}.{extension}"
+        new_filename = f"{file_type}_{current_filename}_{modification_date}.{extension}"
 
     return new_filename
 
 #base_path = "/mnt/c/Users/estev/OneDrive - Universidad Nacional de Colombia/Universidad/02_Física/SemestreVII/IntroducciónCienciasDeLaComputación/Proyecto/"
 #origin_path = "Test_folder"
 
-base_path = "Test_folder/"
+def is_file_for_delete(file_path, reference_name):
+    """
+    Checks if user wants to delete given file
+    Returns:
+        flag (Bool)
+    """
+    print(f"Desea eliminar el archivo {reference_name}")
+    flag = int(input("Ingrese 0 para no y 1 para sí: "))
+    
+    return flag
 
-file_list = [p for p in glob.glob(base_path + "**", recursive = True) if os.path.isfile(p)]
+
+# Main
+
+base_path = "descargas/"
+
+file_list = get_files_path_list(base_path)
+
+image_counter = 0
 
 for file in file_list:
     file_type = get_document_type(file, extensions_type)
 
+    if file_type == "imagen":
+        image_counter += 1
+
+    new_filename = give_new_filename(file_type, file, image_counter)
+
     if (is_file_old(file)):
-      destination_directory = base_path + "old_files" 
+      if is_file_for_delete(file, new_filename):
+          os.remove(file)
+          continue
+      else:
+        destination_directory = base_path + "old_files" 
     else:
        destination_directory = base_path + file_type
-    
+
     try:
        os.mkdir(destination_directory)
     except FileExistsError:
-       pass  
-    
-    new_filename = give_new_filename(file_type, file)
+       pass   
 
     shutil.move(file, f"{destination_directory}/{new_filename}")
 
@@ -103,6 +140,8 @@ for file in file_list:
        os.rmdir(os.path.dirname(file))
     except OSError as e:
        pass
+
+
 
     
     
