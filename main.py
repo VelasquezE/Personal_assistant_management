75cc2bf1a4509dd18a493a922c48aeaddde25de8
VelasquezE <132410679+VelasquezE@users.noreply.github.com>
2025-07-11 16:18:23 -0500

    Test function for second evaluation

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from file_organizer import downloads as fd
 from pdf_manager import encrypter as ec
+from pdf_manager import renamer as ren
 
-#flag = int(input("¿Desea organizar la carpeta de descargas? "))
+print("Bienvenido a la demostración del asistente personal \n")
+print("Seleccione la opción que desea probar: \n")
+print("1. Organizar carpeta \n")
+print("2. Encriptar pdf \n")
+print("3. Renombrar pdf \n")
 
-#if flag:
-    #fd.organize_download_folder("/mnt/c/Users/estev/OneDrive - Universidad Nacional de Colombia/Universidad/02_Física/SemestreVII/IntroducciónCienciasDeLaComputación/Proyecto/Personal_assistant_management/test/")
-    #print("Carpeta de descargas organizada")
+option = int(input())
 
-flag = int(input("¿Desea encriptar un pdf?"))
+if (option == 1):
+    fd.organize_download_folder("/mnt/c/Users/estev/OneDrive - Universidad Nacional de Colombia/Universidad/02_Física/SemestreVII/IntroducciónCienciasDeLaComputación/Proyecto/test/")
+    print("Carpeta de descargas organizada")
 
-if flag:
+if (option == 2):
     ec.encrypt_pdf()
     print("PDF encriptado con éxito")
 
-
+if (option == 3):
+    ren.rename_pdf()
+    print("El PDF ha sido renombrado")
